339c1824e9530b04c118823e4cb1e45d42030b74
Anthony Watson <60107609+anwatson1982@users.noreply.github.com>
2020-11-19 22:17:24 +0000

    Update Customer.cs
    Updated customer class

--- Lashes_CRM/Lashes_CRM/Customer.cs
+++ Lashes_CRM/Lashes_CRM/Customer.cs
@@ -3,19 +3,22 @@ namespace Lashes_CRM
 {
     public class Customer
     {
+       
+       public int CustomerID;
+       public string FirstName;
+       public String LastName;
+       public string PhoneNumber;
+       public string EmailAddress;
+       public DateTime date;
+       public string LashType;
+       public string LashSize;
+       public string TreatmentType;
+       public bool ActiveUser;
+       public bool SubscriberList;
+
         public Customer()
         {
-            int CustomerID;
-            string FirstName;
-            String LastName;
-            string PhoneNumber;
-            string EmailAddress;
-            DateTime date;
-            string LashType;
-            string LashSize;
-            string TreatmentType;
-            bool ActiveUser;
-            bool SubscriberList;
+
         }
     }
 }
