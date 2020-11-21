fee7234ccce568147bc56aa10eea6220da78b744
Admin <Admin@DESKTOP-7DTJAM9>
2020-11-21 23:08:07 +0000

    Added Serlizer to programs.cs

--- Lashes_CRM/Lashes_CRM/Program.cs
+++ Lashes_CRM/Lashes_CRM/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace Lashes_CRM
 {
     class Program
     {
+        public static object XmlSerializer { get; private set; }
+        public static object Database { get; private set; }
+
         static void Main(string[] args)
         {
             List<string> LashType = new List<string>();
@@ -24,6 +29,25 @@ namespace Lashes_CRM
             TreatmentType.Add("21");
             TreatmentType.Add("22");
 
+            Console.WriteLine($"Enter first name");
+            string FirstNameInput = Console.ReadLine();
+
+            Console.WriteLine($"Enter surname name");
+            string SurNameInput = Console.ReadLine();
+
+
+            Customer customer1 = new Customer() { FirstName = FirstNameInput, LastName = SurNameInput };
+            Console.WriteLine(customer1);
+            
+
+            //Write Customer date to xml document 
+
+            
+            Customer Database = new Customer();
+            XmlSerializer xs = new XmlSerializer(typeof(Customer));
+            TextWriter txtWriter = new StreamWriter(@"C:\Database.xml");
+            xs.Serialize(txtWriter, Database);
+            txtWriter.Close();
         }
     }
 }
